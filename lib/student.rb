9e6baa4a8e63b3c5800fd7449fa9fac2c49f69da
Zac Willmington <zac.willmington@gmail.com>
2018-03-26 23:24:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,14 +5,13 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-     # binding.pry
     @name = student_hash[:name]
     @location = student_hash[:location]
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+      
   end
 
   def add_student_attributes(attributes_hash)
