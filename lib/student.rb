765309859a7674a27775f001b3c34391c647f841
Zac Willmington <zac.willmington@gmail.com>
2018-03-26 23:36:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+      attributes_hash.each do |attributes|
+          @twitter = attributes[:twitter]
+      end
   end
 
   def self.all
