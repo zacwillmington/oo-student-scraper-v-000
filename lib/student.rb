9121a31b08c19b014d4068539efeb3896102ced9
Zac Willmington <zac.willmington@gmail.com>
2018-03-26 23:26:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+      binding.pry
+      students_array.each do |student|
+          student[]
+      end
       
   end
 
